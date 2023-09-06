142e1aee63e96014d96726ed26643e0d44c7f070
Saikrishna Arcot <sarcot@microsoft.com>
2023-09-06 11:28:12 -0700

    Update bitarray to 2.8.1 and pyyaml to 6.0 for sonic-config-engine
    Signed-off-by: Saikrishna Arcot <sarcot@microsoft.com>

--- src/sonic-config-engine/setup.py
+++ src/sonic-config-engine/setup.py
@@ -11,11 +11,10 @@ sonic_dependencies = ['sonic-py-common']
 
 # Common dependencies for Python 2 and 3
 dependencies = [
-    'bitarray==1.5.3',
+    'bitarray==2.8.1',
     'ipaddress==1.0.23',
     'lxml==4.9.1',
     'netaddr==0.8.0',
-    'pyyaml==5.4.1',
 ]
 
 if sys.version_info.major == 3:
@@ -26,6 +25,7 @@ if sys.version_info.major == 3:
         # dependencies section of setuptools followed by uninstall of enum43
         # 'pyangbind==0.8.1',
         'Jinja2>=2.10',
+        'pyyaml==6.0.1',
     ]
     sonic_dependencies += [
         'sonic-yang-mgmt>=1.0',
@@ -41,7 +41,9 @@ else:
         'pyangbind==0.6.0',
         'zipp==1.2.0',  # importlib-resources needs zipp and seems to have a bug where it will try to install too new of a version for Python 2
         'importlib-resources==3.3.1',  # importlib-resources v4.0.0 was released 2020-12-23 and drops support for Python 2
-        'contextlib2==0.6.0.post1'
+        'contextlib2==0.6.0.post1',
+        # PyYAML 6.0 and newer dropped support for Python 2.7
+        'pyyaml==5.4.1',
     ]
 
 # Common modules for python2 and python3
