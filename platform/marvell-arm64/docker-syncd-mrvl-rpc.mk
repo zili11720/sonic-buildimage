c6602c95855bac27e741d5b60caf9e40838f8e16
Pavan Naregundi <92989231+pavannaregundi@users.noreply.github.com>
2024-02-13 04:41:19 +0530

    [Marvell-arm64]: Fix SYNCD_RPC build (#17266)
    Change-Id: I0bd4932d03141f3f7bc523b49a1bf3d1809817a8
    
    Signed-off-by: Pavan Naregundi <pnaregundi@marvell.com>

--- platform/marvell-arm64/docker-syncd-mrvl-rpc.mk
+++ platform/marvell-arm64/docker-syncd-mrvl-rpc.mk
@@ -23,3 +23,5 @@ $(DOCKER_SYNCD_MRVL_RPC)_RUN_OPT += --privileged -t
 $(DOCKER_SYNCD_MRVL_RPC)_RUN_OPT += -v /host/machine.conf:/etc/machine.conf
 $(DOCKER_SYNCD_MRVL_RPC)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
 $(DOCKER_SYNCD_MRVL_RPC)_RUN_OPT += -v /host/warmboot:/var/warmboot
+
+SONIC_BULLSEYE_DOCKERS += $(DOCKER_SYNCD_MRVL_RPC)
