8506826348c422be191599572404eab9af89638c
Saikrishna Arcot <sarcot@microsoft.com>
2024-02-21 13:22:07 -0800

    Update the database, teamd, and radv containers to Bookworm (#18108)
    * Update Redis DB start options for multi-asic and chassis cases
    
    Starting with Redis 7.0 (specifically, redis/redis#9034), setting a
    custom `--bind` address on the command line no longer disables protected
    mode (which blocks connections from non-localhost IP addresses unless a
    password is set). For multi-asic and chassis database DBs, we currently
    specify a non-localhost IP address and do not specify any password,
    which means this change would break things there.
    
    To work around this, if we are specifying a non-localhost IP address on
    the command line, then disable protected mode.
    
    * Clean up debug pacakge list for docker-teamd
    
    The debug pacakges for swss and libswsscommon are already installed by
    docker-swss-layer-bookworm and docker-config-engine-bookworm, so they
    don't need to be specified here again.
    
    Signed-off-by: Saikrishna Arcot <sarcot@microsoft.com>

--- rules/libteam.mk
+++ rules/libteam.mk
@@ -1,32 +1,34 @@
 # libteam packages
 
-LIBTEAM_VERSION = 1.30-1
+LIBTEAM_VERSION := 1.31
+LIBTEAM_VERSION_FULL := $(LIBTEAM_VERSION)-1
 
 export LIBTEAM_VERSION
+export LIBTEAM_VERSION_FULL
 
-LIBTEAM = libteam5_$(LIBTEAM_VERSION)_$(CONFIGURED_ARCH).deb
+LIBTEAM = libteam5_$(LIBTEAM_VERSION_FULL)_$(CONFIGURED_ARCH).deb
 $(LIBTEAM)_SRC_PATH = $(SRC_PATH)/libteam
 $(LIBTEAM)_DEPENDS += $(LIBNL_GENL3_DEV) $(LIBNL_CLI_DEV)
 SONIC_MAKE_DEBS += $(LIBTEAM)
 
-LIBTEAM_DBG = libteam5-dbgsym_$(LIBTEAM_VERSION)_$(CONFIGURED_ARCH).deb
+LIBTEAM_DBG = libteam5-dbgsym_$(LIBTEAM_VERSION_FULL)_$(CONFIGURED_ARCH).deb
 $(eval $(call add_derived_package,$(LIBTEAM),$(LIBTEAM_DBG)))
 
-LIBTEAM_DEV = libteam-dev_$(LIBTEAM_VERSION)_$(CONFIGURED_ARCH).deb
+LIBTEAM_DEV = libteam-dev_$(LIBTEAM_VERSION_FULL)_$(CONFIGURED_ARCH).deb
 $(LIBTEAM_DEV)_DEPENDS += $(LIBTEAMDCTL)
 $(eval $(call add_derived_package,$(LIBTEAM),$(LIBTEAM_DEV)))
 
-LIBTEAMDCTL = libteamdctl0_$(LIBTEAM_VERSION)_$(CONFIGURED_ARCH).deb
+LIBTEAMDCTL = libteamdctl0_$(LIBTEAM_VERSION_FULL)_$(CONFIGURED_ARCH).deb
 $(eval $(call add_derived_package,$(LIBTEAM),$(LIBTEAMDCTL)))
 
-LIBTEAMDCTL_DBG = libteamdctl0-dbgsym_$(LIBTEAM_VERSION)_$(CONFIGURED_ARCH).deb
+LIBTEAMDCTL_DBG = libteamdctl0-dbgsym_$(LIBTEAM_VERSION_FULL)_$(CONFIGURED_ARCH).deb
 $(eval $(call add_derived_package,$(LIBTEAM),$(LIBTEAMDCTL_DBG)))
 
-LIBTEAM_UTILS = libteam-utils_$(LIBTEAM_VERSION)_$(CONFIGURED_ARCH).deb
+LIBTEAM_UTILS = libteam-utils_$(LIBTEAM_VERSION_FULL)_$(CONFIGURED_ARCH).deb
 $(LIBTEAM_UTILS)_DEPENDS += $(LIBTEAMDCTL)
 $(eval $(call add_derived_package,$(LIBTEAM),$(LIBTEAM_UTILS)))
 
-LIBTEAM_UTILS_DBG = libteam-utils-dbgsym_$(LIBTEAM_VERSION)_$(CONFIGURED_ARCH).deb
+LIBTEAM_UTILS_DBG = libteam-utils-dbgsym_$(LIBTEAM_VERSION_FULL)_$(CONFIGURED_ARCH).deb
 $(eval $(call add_derived_package,$(LIBTEAM),$(LIBTEAM_UTILS_DBG)))
 
 # The .c, .cpp, .h & .hpp files under src/{$DBG_SRC_ARCHIVE list}
