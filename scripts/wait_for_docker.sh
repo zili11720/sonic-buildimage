7ef22fdcb38f9317c486561db3a95ad5d075604e
Christian Svensson <blue@cmd.nu>
2024-03-30 08:49:47 +0100

    [build] verbose output in wait_for_docker.sh when timing out (#17869)
    If there is some problem starting the internal dockerd currently the only output in the build output is 60x `errors pretty printing info`. This comes from the stderr on the `docker info` invocations and is not helpful to the user.
    The final error becomes `make: *** [slave.mk:918: docker-start] Error 1`.
    
    Instead, hide the `errors pretty printing info` messages and print the log of dockerd if we time out.
    
    Signed-off-by: Christian Svensson <blue@cmd.nu>

--- scripts/wait_for_docker.sh
+++ scripts/wait_for_docker.sh
@@ -3,7 +3,7 @@
 total_time=$1
 cnt=0
 while [ $cnt -le $total_time ]; do
-    docker info 1>/dev/null
+    docker info &>/dev/null
     rv=$?
     if [ $rv -eq 0 ]; then
         exit 0
@@ -11,5 +11,10 @@ while [ $cnt -le $total_time ]; do
     sleep 1
     cnt=$((cnt+1))
 done
+
+echo 'Timed out waiting for internal docker daemon to start' > /dev/stderr
+echo '==== START OF /var/log/docker.log ===='
+cat /var/log/docker.log
+echo '==== END OF /var/log/docker.log ===='
 
 exit 1
