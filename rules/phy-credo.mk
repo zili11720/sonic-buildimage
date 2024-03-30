7bd83ccbdd07f322399d17d5330a162d071d4ad4
byu343 <byu@arista.com>
2024-03-30 00:15:31 -0700

    [Arista] Update phy-credo package for bookworm (#18464)
    For the debian bookworm, we fixed some build issue of phy-credo package and packaged it for the bookworm.

--- rules/phy-credo.mk
+++ rules/phy-credo.mk
@@ -1,3 +1,3 @@
 PHY_CREDO = phy-credo_1.0_amd64.deb
-$(PHY_CREDO)_URL = "https://github.com/aristanetworks/sonic-firmware/raw/446f30ccd8626f904d89d5798da7294948e090a6/phy/phy-credo_1.0_amd64.deb"
+$(PHY_CREDO)_URL = "https://github.com/aristanetworks/sonic-firmware/raw/9e34da2a2d2d1e7e972cda3064b7b73c0558b322/phy/phy-credo_1.0_amd64.deb"
 SONIC_ONLINE_DEBS += $(PHY_CREDO)
