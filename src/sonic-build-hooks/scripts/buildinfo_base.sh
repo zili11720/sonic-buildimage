c3899ef2eadaddf8929b60787739c91287b937e7
Liu Shilong <shilongliu@microsoft.com>
2024-04-25 10:49:06 +0800

    [build] Fix bookworm reproducible build issue. (#18789)
    Why I did it
    Bookworm involved new file /etc/apt/sources.list.d/debian.sources to setup debian mirror repo.
    Reproducible build need to support this scenario.
    
    Work item tracking
    Microsoft ADO (number only): 27792318
    How I did it
    How to verify it

--- src/sonic-build-hooks/scripts/buildinfo_base.sh
+++ src/sonic-build-hooks/scripts/buildinfo_base.sh
@@ -123,6 +123,12 @@ set_reproducible_mirrors()
         expression2="/^#*deb.*$REPR_MIRROR_URL_PATTERN/! s/^#\s*(#*deb)/\1/"
         expression3="/#SET_REPR_MIRRORS/d"
     fi
+    if [[ "$1" != "-d" ]] && [ -f /etc/apt/sources.list.d/debian.sources ]; then
+        mv /etc/apt/sources.list.d/debian.sources /etc/apt/sources.list.d/debian.sources.back
+    fi
+    if [[ "$1" == "-d" ]] && [ -f /etc/apt/sources.list.d/debian.sources.back ]; then
+        mv /etc/apt/sources.list.d/debian.sources.back /etc/apt/sources.list.d/debian.sources
+    fi
 
     local mirrors="/etc/apt/sources.list $(find /etc/apt/sources.list.d/ -type f)"
     for mirror in $mirrors; do
