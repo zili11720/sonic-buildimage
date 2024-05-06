33990d88e1273a59893c89e4d2a770a86aa50042
vidhya-rajan <121014183+vidhya-rajan@users.noreply.github.com>
2024-05-06 21:40:01 +0530

    Platform driver change for Bookworm (#17702)

--- platform/innovium/sonic-platform-modules-cel/midstone-200i/modules/mc24lc64t.c
+++ platform/innovium/sonic-platform-modules-cel/midstone-200i/modules/mc24lc64t.c
@@ -108,7 +108,7 @@ static int mc24lc64t_probe(struct i2c_client *client,
         return err;
 }
 
-static int mc24lc64t_remove(struct i2c_client *client)
+static void mc24lc64t_remove(struct i2c_client *client)
 {
         struct mc24lc64t_data *drvdata = i2c_get_clientdata(client);
 
@@ -116,7 +116,7 @@ static int mc24lc64t_remove(struct i2c_client *client)
 
         sysfs_remove_bin_file(&client->dev.kobj, &mc24lc64t_bit_attr);
 
-        return 0;
+        return ;
 }
 
 static const struct i2c_device_id mc24lc64t_id[] = {
