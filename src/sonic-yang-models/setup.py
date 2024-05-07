afdc5d02aa1297cf761f35e07a1d889bfdecfb95
davidpil2002 <91657985+davidpil2002@users.noreply.github.com>
2024-05-07 13:55:58 +0300

    Add LDAP feature support (#16969)
    Why I did it
    To support LDAP feature
    
    - How I did it
    Similar to Radius/Tacacs authentication methods, the SONiC device is the LDAP client.
    Installed the Debian LDAP packages related to making SONiC able to function as an LDAP client.
    
    More description in the following HLD: sonic-net/SONiC#1487
    
    - How to verify it
    Do LDAP configuration according to the HLD, then connect to the SONiC switch by using a user that exists in your LDAP server.

--- src/sonic-yang-models/setup.py
+++ src/sonic-yang-models/setup.py
@@ -164,6 +164,7 @@ setup(
                          './yang-models/sonic-system-aaa.yang',
                          './yang-models/sonic-system-tacacs.yang',
                          './yang-models/sonic-system-radius.yang',
+                         './yang-models/sonic-system-ldap.yang',
                          './yang-models/sonic-telemetry.yang',
                          './yang-models/sonic-telemetry_client.yang',
                          './yang-models/sonic-gnmi.yang',
