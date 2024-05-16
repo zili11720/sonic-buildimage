4863b0b3ea56f8fc4cf167ba05d8e80263a83051
Saikrishna Arcot <sarcot@microsoft.com>
2024-05-16 16:54:53 -0700

    (Properly) upgrade docker-restapi to Bookworm, and add a debug container (#18975)
    Upgrade docker-restapi container to Bookworm. It's worth noting that
    because this container was not in the SONIC_BULLSEYE_DOCKERS list, the
    binaries/libraries that got packaged here came from the Bookworm build,
    but the base layer was for Bullseye, so it was already in some hybrid
    state.
    
    Also, include the autogenerated dbgsym package for restapi in
    `rules/restapi.mk`, and add a debug container for restapi.
    
    Also, enable caching of docker-restapi (not sure why it wasn't enabled
    before).
    
    Signed-off-by: Saikrishna Arcot <sarcot@microsoft.com>

--- rules/restapi.mk
+++ rules/restapi.mk
@@ -2,8 +2,12 @@
 
 RESTAPI = sonic-rest-api_1.0.1_$(CONFIGURED_ARCH).deb
 $(RESTAPI)_SRC_PATH = $(SRC_PATH)/sonic-restapi
+
 $(RESTAPI)_DEPENDS += $(LIBNL3_DEV) $(LIBNL_GENL3_DEV) \
                             $(LIBNL_ROUTE3_DEV) $(LIBSWSSCOMMON_DEV) $(LIBSWSSCOMMON)
 $(RESTAPI)_RDEPENDS += $(LIBNL3) $(LIBNL_GENL3) \
                              $(LIBNL_ROUTE3) $(LIBSWSSCOMMON)
 SONIC_DPKG_DEBS += $(RESTAPI)
+
+RESTAPI_DBG = sonic-rest-api-dbgsym_1.0.1_$(CONFIGURED_ARCH).deb
+$(eval $(call add_derived_package,$(RESTAPI_DBG),$(RESTAPI_DBG)))
