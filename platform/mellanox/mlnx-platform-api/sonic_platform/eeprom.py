c198057fad0caed1a908c775842a33cb4911c7f4
Junchao-Mellanox <57339448+Junchao-Mellanox@users.noreply.github.com>
2024-05-29 18:30:36 +0800

    [Mellanox] enlarge the eeprom ready timeout from 20 to 60 (#18983)
    - Why I did it
    Service like ZTP accesses EEPROM very early at the boot stage. It could cause an issue that EEPROM is not ready. The PR enlarge EEPROM ready timeout from 20s to 60s.
    
    - How I did it
    Enlarge EEPROM ready timeout from 20s to 60s.
    
    - How to verify it
    Manual test

--- platform/mellanox/mlnx-platform-api/sonic_platform/eeprom.py
+++ platform/mellanox/mlnx-platform-api/sonic_platform/eeprom.py
@@ -52,7 +52,7 @@ if platform_name and 'simx' in platform_name and not platform_name in platform_s
             os.makedirs(os.path.dirname(EEPROM_SYMLINK))
         subprocess.check_call(['/usr/bin/xxd', '-r', '-p', 'syseeprom.hex', EEPROM_SYMLINK], cwd=platform_path)
 
-WAIT_EEPROM_READY_SEC = 20
+WAIT_EEPROM_READY_SEC = 60
 
 
 class Eeprom(eeprom_tlvinfo.TlvInfoDecoder):
