339a4e62ce88c90b3fde5b2e192576740f2ce757
abdosi <58047199+abdosi@users.noreply.github.com>
2024-07-12 19:29:51 -0700

    [healthd] Added change to handle Feature Table exception (#19480)
    *Fix: #18818
    
    Handle any exception in API get_service_from_feature_table() gracefully .
    
    ---------
    
    Signed-off-by: Abhishek Dosi <abdosi@microsoft.com>

--- src/system-health/health_checker/sysmonitor.py
+++ src/system-health/health_checker/sysmonitor.py
@@ -204,20 +204,23 @@ class Sysmonitor(ProcessTaskBase):
 
         while max_retry > 0:
             success = True
-            feature_table = self.config_db.get_table("FEATURE")
-            device_config = {}
-            device_config['DEVICE_METADATA'] = self.config_db.get_table('DEVICE_METADATA')
-            device_config.update(device_info.get_device_runtime_metadata())
-            for srv, fields in feature_table.items():
-                if 'state' not in fields:
-                    success = False
-                    logger.log_warning("FEATURE table is not fully ready: {}, retrying".format(feature_table))
-                    break
-                state = self.get_render_value_for_field(fields["state"], device_config, ['enabled', 'disabled', 'always_enabled', 'always_disabled'])
-                if state not in ["disabled", "always_disabled"]:
-                    srvext = srv + ".service"
-                    if srvext not in dir_list:
-                        dir_list.append(srvext)
+            try:
+                feature_table = self.config_db.get_table("FEATURE")
+                device_config = {}
+                device_config['DEVICE_METADATA'] = self.config_db.get_table('DEVICE_METADATA')
+                device_config.update(device_info.get_device_runtime_metadata())
+                for srv, fields in feature_table.items():
+                    if 'state' not in fields:
+                        success = False
+                        logger.log_warning("FEATURE table is not fully ready: {}, retrying".format(feature_table))
+                        break
+                    state = self.get_render_value_for_field(fields["state"], device_config, ['enabled', 'disabled', 'always_enabled', 'always_disabled'])
+                    if state not in ["disabled", "always_disabled"]:
+                        srvext = srv + ".service"
+                        if srvext not in dir_list:
+                            dir_list.append(srvext)
+            except:
+                success = False
             if not success:
                 max_retry -= 1
                 time.sleep(retry_delay)
