8a0beaf33167898b1d086571bda7c29b88e7a9c0
Yuanzhe <150663541+yuazhe@users.noreply.github.com>
2024-07-29 14:19:46 +0800

    [Mellanox] fix sfp eeprom unreadable after switching from SW to FW control mode (#19563)
    - Why I did it
    The reading of eeprom instantly after switch the module from sw control to fw control might fail, so add a delay
    
    Signed-off-by: Yuanzhe, Liu <yualiu@nvidia.com>

--- platform/mellanox/mlnx-platform-api/sonic_platform/sfp.py
+++ platform/mellanox/mlnx-platform-api/sonic_platform/sfp.py
@@ -500,6 +500,21 @@ class SFP(NvidiaSFPCommon):
             return False
         eeprom_raw = self._read_eeprom(0, 1, log_on_error=False)
         return eeprom_raw is not None
+    
+    @classmethod
+    def wait_sfp_eeprom_ready(cls, sfp_list, wait_time):
+        not_ready_list = sfp_list
+        
+        while wait_time > 0:
+            not_ready_list = [s for s in not_ready_list if s.state == STATE_FW_CONTROL and s._read_eeprom(0, 2,False) is None]
+            if not_ready_list:
+                time.sleep(0.1)
+                wait_time -= 0.1
+            else:
+                return
+        
+        for s in not_ready_list:
+            logger.log_error(f'SFP {s.sdk_index} eeprom is not ready')
 
     # read eeprom specfic bytes beginning from offset with size as num_bytes
     def read_eeprom(self, offset, num_bytes):
@@ -1724,7 +1739,8 @@ class SFP(NvidiaSFPCommon):
                 logger.log_error(f'SFP {index} is not in stable state after initializing, state={s.state}')
             logger.log_notice(f'SFP {index} is in state {s.state} after module initialization')
 
-
+        cls.wait_sfp_eeprom_ready(sfp_list, 2)
+        
 class RJ45Port(NvidiaSFPCommon):
     """class derived from SFP, representing RJ45 ports"""
 
