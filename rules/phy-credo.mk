d004a49935a6ef6f299f77511b3da9ba3f33cbdc
wenyiz2021 <91497961+wenyiz2021@users.noreply.github.com>
2024-08-20 12:21:25 -0700

    [SAI 11] [Arista] Update phy-credo.mk (#19958)
    it's needed for Arista CW2 linecard for SAI11.
    
    There is system side mechanism in phy-credo.py to work around the links down issue on CW2 linecard.
    This PR is changing $(PHY_CREDO)_URL to point to phy-credo_1.0_amd64.deb that is already merged with the fix in aristanetworks/sonic-firmware#2
    
    Verified links are all up with credo debian update, without this change, 1 portchannel is down on Arista CL2 LC.

--- rules/phy-credo.mk
+++ rules/phy-credo.mk
@@ -1,3 +1,3 @@
 PHY_CREDO = phy-credo_1.0_amd64.deb
-$(PHY_CREDO)_URL = "https://github.com/aristanetworks/sonic-firmware/raw/9e34da2a2d2d1e7e972cda3064b7b73c0558b322/phy/phy-credo_1.0_amd64.deb"
+$(PHY_CREDO)_URL = "https://github.com/aristanetworks/sonic-firmware/raw/24716c4e03f223d8e18afff786ac427f6ac77fe0/phy/phy-credo_1.0_amd64.deb"
 SONIC_ONLINE_DEBS += $(PHY_CREDO)
