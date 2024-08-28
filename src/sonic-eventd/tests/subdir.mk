93f3cad6faaf0be67548c4e99a66ffedad98ca52
Liu Shilong <shilongliu@microsoft.com>
2024-08-29 04:06:32 +0800

    Revert "Event  and alarm management (#17949)" (#20052)
    This reverts commit 10f0fe8d8c22f41ae24a264a6e0298864c8eb755.

--- src/sonic-eventd/tests/subdir.mk
+++ src/sonic-eventd/tests/subdir.mk
@@ -1,10 +1,8 @@
 CC := g++
 
-TEST_OBJS += ./tests/eventd_ut.o  ./tests/main.o 
+TEST_OBJS += ./tests/eventd_ut.o ./tests/main.o
 
-C_DEPS += ./tests/eventd_ut.d ./tests/main.d ./tests/eventdb_ut.d
-
-EVENTDB_TEST_OBJS += ./tests/eventdb_ut.o
+C_DEPS += ./tests/eventd_ut.d ./tests/main.d
 
 tests/%.o: tests/%.cpp
 	@echo 'Building file: $<'
@@ -12,4 +10,3 @@ tests/%.o: tests/%.cpp
 	$(CC) -D__FILENAME__="$(subst tests/,,$<)" $(CFLAGS) -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
-
