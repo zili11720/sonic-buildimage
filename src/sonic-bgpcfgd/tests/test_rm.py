3cc3e1a11f162a34d43f275fd2af5e148578ff9c
siqbal1986 <shahzad.iqbal@gmail.com>
2024-08-29 19:14:13 -0700

    [Bug Fix] Route map for BGP profile FROM_SDN_APPLIANCE_ROUTES (#19871)
    Why I did it
    This is a bug fix and this change should go into 202305 brranch and onwards.
    Added Route map support for BGP profile FROM_SDN_APPLIANCE_ROUTES
    With this change the following route map should be added if the BGP profile is added
    
    route-map FROM_SDN_APPLIANCE_ROUTES_RM permit 100
    set as-path prepend <Prepend value of FROM_SDN_SLB_DEPLOYMENT_ID>
    set community <community id>
    set origin incomplete
    Work item tracking
    Microsoft ADO (number only):
    28896695
    How I did it
    How to verify it
    Added test to verify the rotue map creation.

--- src/sonic-bgpcfgd/tests/test_rm.py
+++ src/sonic-bgpcfgd/tests/test_rm.py
@@ -25,7 +25,7 @@ def constructor():
     mgr = RouteMapMgr(common_objs, "APPL_DB", "BGP_PROFILE_TABLE")
     return mgr
 
-def set_del_test(mgr, op, args, expected_ret, expected_cmds):
+def set_del_test(mgr, op, cfg_args, expected_ret, expected_cmds):
     set_del_test.push_list_called = False
     def push_list(cmds):
         set_del_test.push_list_called = True
@@ -34,10 +34,12 @@ def set_del_test(mgr, op, args, expected_ret, expected_cmds):
     mgr.cfg_mgr.push_list = push_list
 
     if op == "SET":
-        ret = mgr.set_handler(*args)
-        assert ret == expected_ret
+        for i in range(0, len(cfg_args)):
+            ret = mgr.set_handler(cfg_args[i][0], cfg_args[i][1])
+            assert ret == expected_ret
     elif op == "DEL":
-        mgr.del_handler(*args)
+        for i in range(0, len(cfg_args)):               
+            mgr.del_handler(cfg_args[i])
     else:
         assert False, "Wrong operation"
 
@@ -51,9 +53,9 @@ def test_set_del():
     set_del_test(
         mgr,
         "SET",
-        ("FROM_SDN_SLB_ROUTES", {
+        [("FROM_SDN_SLB_ROUTES", {
             "community_id": "1234:1234"
-        }),
+        })],
         True,
         [
             ["route-map FROM_SDN_SLB_ROUTES_RM permit 100",
@@ -72,3 +74,68 @@ def test_set_del():
             ["no route-map FROM_SDN_SLB_ROUTES_RM permit 100"]
         ]
     )
+
+def test_set_del_com_sdn_apl():
+    mgr = constructor()
+    set_del_test(
+        mgr,
+        "SET",
+        [("FROM_SDN_APPLIANCE_ROUTES", {
+            "community_id": "1235:1235"
+        })],
+        True,
+        [
+            ["route-map FROM_SDN_APPLIANCE_ROUTES_RM permit 100",
+             " set as-path prepend 12346 12346",
+             " set community 1235:1235",
+             " set origin incomplete"]
+        ]
+    )
+
+    set_del_test(
+        mgr,
+        "DEL",
+        ("FROM_SDN_APPLIANCE_ROUTES",),
+        True,
+        [
+            ["no route-map FROM_SDN_APPLIANCE_ROUTES_RM permit 100"]
+        ]
+    )
+
+def test_set_del_com_sdn_apl_and_slb():
+    mgr = constructor()
+    set_del_test(
+        mgr,
+        "SET",
+        [("FROM_SDN_APPLIANCE_ROUTES", {
+            "community_id": "1235:1235"
+        }),
+        ("FROM_SDN_SLB_ROUTES", {
+            "community_id": "1234:1234"
+        }
+        )],
+        True,
+        [
+            ["route-map FROM_SDN_APPLIANCE_ROUTES_RM permit 100",
+             " set as-path prepend 12346 12346",
+             " set community 1235:1235",
+             " set origin incomplete"],
+           ["route-map FROM_SDN_SLB_ROUTES_RM permit 100",
+             " set as-path prepend 12346 12346",
+             " set community 1234:1234",
+             " set origin incomplete"]
+                   
+        ]
+    )
+
+    set_del_test(
+        mgr,
+        "DEL",
+        ("FROM_SDN_APPLIANCE_ROUTES", "FROM_SDN_SLB_ROUTES",),
+        True,
+        [
+            ["no route-map FROM_SDN_APPLIANCE_ROUTES_RM permit 100"],
+            ["no route-map FROM_SDN_SLB_ROUTES_RM permit 100"]
+        ] 
+    )
+
