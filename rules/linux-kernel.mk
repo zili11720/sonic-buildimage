219da22be0426f07e182b1e3dc5f03db698dacbd
Saikrishna Arcot <sarcot@microsoft.com>
2024-08-30 18:04:26 -0700

    [debian]: Update to Linux 6.1.94 (#19564)
    * Update to Linux 6.1.94
    * Integrate HW-MGMT 7.0040.1008 Changes (#17)
    * Update DNX kernel module build
    * Update kernel and saibcm-modules-dnx to versions on branch
    
    Signed-off-by: Saikrishna Arcot <sarcot@microsoft.com>
    Co-authored-by: Vivek <vivekreddykarri98@gmail.com>

--- rules/linux-kernel.mk
+++ rules/linux-kernel.mk
@@ -1,9 +1,9 @@
 # linux kernel package
 
-KVERSION_SHORT = 6.1.0-11-2
+KVERSION_SHORT = 6.1.0-22-2
 KVERSION = $(KVERSION_SHORT)-$(CONFIGURED_ARCH)
-KERNEL_VERSION = 6.1.38
-KERNEL_SUBVERSION = 4
+KERNEL_VERSION = 6.1.94
+KERNEL_SUBVERSION = 1
 ifeq ($(CONFIGURED_ARCH), armhf)
 # Override kernel version for ARMHF as it uses arm MP (multi-platform) for short version
 KVERSION = $(KVERSION_SHORT)-armmp
