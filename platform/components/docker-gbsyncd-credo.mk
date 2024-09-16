058e0997a5e5efe76c3e8ff1be5cc1edf6508443
Junhua Zhai <junhua.zhai@outlook.com>
2024-09-17 01:03:47 +0800

    [arista]: Upgrade credo sai package to v0.9.9 (#20209)
    Upgrade credo sai package to the latest v0.9.9 to deliver a key fix from Credo to the community 
    or the blackhawk phys used on Arista platform 7060dx5_64s.
    
    ADO: 29387310

--- platform/components/docker-gbsyncd-credo.mk
+++ platform/components/docker-gbsyncd-credo.mk
@@ -1,11 +1,11 @@
 DOCKER_GBSYNCD_PLATFORM_CODE = credo
 
-LIBSAI_CREDO = libsaicredo_0.9.6_amd64.deb
-$(LIBSAI_CREDO)_URL = "https://sonicstorage.blob.core.windows.net/public/credosai/libsaicredo_0.9.6_amd64.deb"
-LIBSAI_CREDO_OWL = libsaicredo-owl_0.9.6_amd64.deb
-$(LIBSAI_CREDO_OWL)_URL = "https://sonicstorage.blob.core.windows.net/public/credosai/libsaicredo-owl_0.9.6_amd64.deb"
-LIBSAI_CREDO_BLACKHAWK = libsaicredo-blackhawk_0.9.6_amd64.deb
-$(LIBSAI_CREDO_BLACKHAWK)_URL = "https://sonicstorage.blob.core.windows.net/public/credosai/libsaicredo-blackhawk_0.9.6_amd64.deb"
+LIBSAI_CREDO = libsaicredo_0.9.9_amd64.deb
+$(LIBSAI_CREDO)_URL = "https://sonicstorage.blob.core.windows.net/public/credosai/$(LIBSAI_CREDO)"
+LIBSAI_CREDO_OWL = libsaicredo-owl_0.9.9_amd64.deb
+$(LIBSAI_CREDO_OWL)_URL = "https://sonicstorage.blob.core.windows.net/public/credosai/$(LIBSAI_CREDO_OWL)"
+LIBSAI_CREDO_BLACKHAWK = libsaicredo-blackhawk_0.9.9_amd64.deb
+$(LIBSAI_CREDO_BLACKHAWK)_URL = "https://sonicstorage.blob.core.windows.net/public/credosai/$(LIBSAI_CREDO_BLACKHAWK)"
 
 ifneq ($($(LIBSAI_CREDO)_URL),)
 include $(PLATFORM_PATH)/../template/docker-gbsyncd-base.mk
