c5dbba33386a1977dbbf6d04ccb47ac913f716cb
Zain Budhwani <99770260+zbud-msft@users.noreply.github.com>
2024-09-23 14:15:45 -0700

    Move eventd enabled check from build time to runtime (#20248)
    ## How I did it
    
    Remove eventd enabled/slim image check from Dockerfile (build time). As part of Dockerfile expose eventd_enabled and slim image flags to ENV which will be used docker_init/start.sh to check if rsyslog plugin should be moved to rsyslog.d
    
    #### How to verify it
    
    Manual test/pipeline

--- dockers/docker-fpm-frr/docker_init.sh
+++ dockers/docker-fpm-frr/docker_init.sh
@@ -111,4 +111,7 @@ TZ=$(cat /etc/timezone)
 rm -rf /etc/localtime
 ln -sf /usr/share/zoneinfo/$TZ /etc/localtime
 
+export EVENTD_STATE=$(sonic-db-cli -s CONFIG_DB HGET 'FEATURE|eventd' 'state')
+j2 -f json --import-env=ENVIRONMENT /usr/share/sonic/templates/rsyslog_plugin/rsyslog_plugin.conf.j2 /usr/share/sonic/templates/rsyslog_plugin/events_info.json  > /etc/rsyslog.d/bgp_events.conf
+
 exec /usr/local/bin/supervisord
