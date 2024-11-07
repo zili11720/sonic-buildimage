679997639ede79a7beef53654be101b14d04b006
JunhongMao <134556118+JunhongMao@users.noreply.github.com>
2024-11-07 13:22:27 -0500

    To fix the issue: show_techsupport & saidump errors during testbed testing by replacing redis-rdb-tool with rdb-cli (#19268)
    * To fix the issue: show_techsupport & saidump errors during testbed testing
    https://github.com/sonic-net/sonic-sairedis/issues/1387
    
    To use the Redis-db SAVE option to save the snapshot of DB each time and recover later, instead of looping through each entry in the table and saving it.
    
    (1) Updated sonic-buildimage repo's platform/broadcom/docker-syncd-brcm-dnx/Dockerfile.j2, 
     install rdb-cli into the syncd containter.
    (2) Updated sonic-sairedis repo's script file: files/scripts/saidump.sh, replace rdbtools with rdb-cli.
    (3) Updated sonic-sairedis repo's saidump/saidump.cpp, to process the rdb-cli's ouput json file.

---
+++ rules/rdb-cli.mk
@@ -0,0 +1,14 @@
+# librdb package
+#
+
+
+RDB_CLI_VERSION_FULL = 0.1
+
+export RDB_CLI_VERSION_FULL
+
+RDB-CLI = rdb-cli
+
+export RDB-CLI
+
+$(RDB-CLI)_SRC_PATH = $(SRC_PATH)/rdb-cli
+SONIC_MAKE_FILES += rdb-cli
