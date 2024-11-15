0811c4dc3572ddb66808a25d77c78ef87fd42671
Kebo Liu <kebol@nvidia.com>
2024-11-16 02:25:47 +0800

    [Mellanox] Add a new reset cause for SN2201 platform (#20363)
    Why I did it
    Add a new reset cause "reset_aux_pwr_or_fu" which was added starting from kernel 5.19 for the SN2201 platform.
    
    How I did it
    Add a new reset cause "reset_aux_pwr_or_fu" in chassis.reboot_major_cause_dict.
    
    How to verify it
    Test the SN2201 platform by triggering a reboot through power-off and verify that the reboot cause is correctly recognized.
    
    Signed-off-by: Kebo Liu <kebol@nvidia.com>

--- platform/mellanox/mlnx-platform-api/sonic_platform/chassis.py
+++ platform/mellanox/mlnx-platform-api/sonic_platform/chassis.py
@@ -1,5 +1,6 @@
 #
-# Copyright (c) 2019-2024 NVIDIA CORPORATION & AFFILIATES.
+# SPDX-FileCopyrightText: NVIDIA CORPORATION & AFFILIATES
+# Copyright (c) 2019-2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -955,6 +956,7 @@ class Chassis(ChassisBase):
         self.reboot_major_cause_dict = {
             'reset_main_pwr_fail'       :   self.REBOOT_CAUSE_POWER_LOSS,
             'reset_aux_pwr_or_ref'      :   self.REBOOT_CAUSE_POWER_LOSS,
+            'reset_aux_pwr_or_fu'       :   self.REBOOT_CAUSE_POWER_LOSS,
             'reset_comex_pwr_fail'      :   self.REBOOT_CAUSE_POWER_LOSS,
             'reset_asic_thermal'        :   self.REBOOT_CAUSE_THERMAL_OVERLOAD_ASIC,
             'reset_comex_thermal'       :   self.REBOOT_CAUSE_THERMAL_OVERLOAD_CPU,
