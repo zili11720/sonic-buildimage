58d7e134ea0a1983e732b2644c71a0403ab7910e
Saikrishna Arcot <sarcot@microsoft.com>
2024-12-10 23:12:48 -0800

    [build]: Add an artificial dependency between two modules for centec-arm64 (#19914)
    For centec-arm64, both platform-modules-fs and platform-modules-e530 try
    to compile the same directory of kernel sources. If both compilations
    are running at the same time, then this is going to lead to errors.
    
    Fix this by introducing an artificial dependency between the two so that
    the compilations are forced to be serialized.
    
    Signed-off-by: Saikrishna Arcot <sarcot@microsoft.com>

--- platform/centec-arm64/platform-modules-fs.mk
+++ platform/centec-arm64/platform-modules-fs.mk
@@ -9,4 +9,8 @@ FS_S5800_48T4S_PLATFORM_MODULE = platform-modules-s5800-48t4s_$(FS_S5800_48T4S_P
 $(FS_S5800_48T4S_PLATFORM_MODULE)_SRC_PATH = $(PLATFORM_PATH)/sonic-platform-modules-fs
 $(FS_S5800_48T4S_PLATFORM_MODULE)_DEPENDS += $(LINUX_HEADERS) $(LINUX_HEADERS_COMMON)
 $(FS_S5800_48T4S_PLATFORM_MODULE)_PLATFORM = arm64-fs_s5800_48t4s-r0
+# Both this and the e530 modules compile ../centec/centec-dal/, which causes a problem
+# if both are being compiled at the same time. To get around that, add an artificial
+# dependency here so that this part is serialized.
+$(FS_S5800_48T4S_PLATFORM_MODULE)_AFTER = $(CENTEC_E530_48T4X_P_PLATFORM_MODULE)
 SONIC_DPKG_DEBS += $(FS_S5800_48T4S_PLATFORM_MODULE)
