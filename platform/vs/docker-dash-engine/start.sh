aeae2684137d3690aa0f323f153e25c1e41d152e
Junhua Zhai <junhua.zhai@outlook.com>
2025-01-18 09:49:10 +0800

    [vs][dash-engine] Ensure attached ports of dash-engine being UP while starting dash-engine (#21452)
    Why I did it
    If the attached ports of dash-engine are not UP, the dash-engine will not be able to receive any packets.
    
    The below is the log when starting up dash-engine without the attached ports being UP:
    
    Calling target program-options parser
    Adding interface eth1 as port 0
    [09:18:14.056] [bmv2] [D] [thread 7] Adding interface eth1 as port 0
    [09:18:14.102] [bmv2] [E] [thread 7] Add port operation failed
    Adding interface eth2 as port 1
    [09:18:14.102] [bmv2] [D] [thread 7] Adding interface eth2 as port 1
    [09:18:14.150] [bmv2] [E] [thread 7] Add port operation failed
    
    Work item tracking
    Microsoft ADO 30887888:
    
    How I did it
    If the attached ports of dash-engine are not UP, ensure them to be UP before starting dash-engine
    
    How to verify it
    The dash-engine runs with ports added successfully.
    
    Adding interface eth1 as port 0
    [09:40:16.810] [bmv2] [D] [thread 11] Adding interface eth1 as port 0
    Adding interface eth2 as port 1
    [09:40:16.863] [bmv2] [D] [thread 11] Adding interface eth2 as port 1

--- platform/vs/docker-dash-engine/start.sh
+++ platform/vs/docker-dash-engine/start.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-simple_switch_grpc --interface 0@eth1 --interface 1@eth2 --log-console --no-p4
+PORT1=eth1
+PORT2=eth2
+
+[[ $(cat /sys/class/net/$PORT1/operstate) != up ]] && ifconfig $PORT1 up
+[[ $(cat /sys/class/net/$PORT2/operstate) != up ]] && ifconfig $PORT2 up
+
+simple_switch_grpc --interface 0@$PORT1 --interface 1@$PORT2 --log-console --no-p4
