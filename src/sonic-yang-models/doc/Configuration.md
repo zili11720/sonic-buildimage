ba601b015d430d60960bac5e0739c4b5b3bd633e
Vasundhara Volam <163894573+vvolam@users.noreply.github.com>
2025-02-05 13:25:47 -0800

    [smartswitch][YANG]Add yang model for DPU_PORT information (#21439)
    Why I did it
    Add yang model for the DPU_PORT container information as below:
    
    DPU_PORT|<PORT_NAME>: 
    state: up|down ; admin state of DPU device 
    vip_ipv4: ipv4 ip ; from minigraph 
    vip_ipv6: ipv6 ip ; from minigraph
    pa_ipv4: ipv4 pa ip ; from minigraph
    pa_ipv6: ipv6 pa ip ; from minigraph
    vdpu_id: id of vdpu ; from minigraph
    gnmi_port: gnmi server port ;
    
    How I did it
    Add new DPU_PORT container to the sonic-smart-switch.yang file.
    
    How to verify it
    Uploaded the new image file to the Smart Switch and confirmed that the YANG model has been updated with the new sonic-smart-switch.yang file.
    
    Description for the changelog
    Add yang model for the DPU_PORT information below:
    
    DPU_PORT|<PORT_NAME>: 
    state: up|down ; admin state of DPU device 
    vip_ipv4: ipv4 ip ; from minigraph 
    vip_ipv6: ipv6 ip ; from minigraph
    pa_ipv4: ipv4 pa ip ; from minigraph
    pa_ipv6: ipv6 pa ip ; from minigraph
    vdpu_id: id of vdpu ; from minigraph
    gnmi_port: gnmi server port ;
    
    Addresses #21141

--- src/sonic-yang-models/doc/Configuration.md
+++ src/sonic-yang-models/doc/Configuration.md
@@ -2973,6 +2973,42 @@ The ASIC_SENSORS table introduces the asic sensors polling configuration when th
 }
 ```
 
+### DPU PORT Configuration^M
+
+The **DPU_PORT** table introduces the configuration for the DPUs(Data Processing Unit) PORT information available on the platform.
+
+```json
+{
+    "DPU_PORT": {
+        "dpu0": {
+            "state": "up",
+            "vip_ipv4": "192.168.1.1",
+            "vip_ipv6": "2001:db8::10",
+            "pa_ipv4": "192.168.1.10",
+            "pa_ipv6": "2001:db8::10",
+            "vdpu_id": "vdpu0",
+            "gnmi_port": "50052"
+        },
+        "dpu1": {
+            "state": "down",
+            "vip_ipv4": "192.168.1.2",
+            "vip_ipv6": "2001:db8::20",
+            "pa_ipv4": "192.168.1.20",
+            "pa_ipv6": "2001:db8::20",
+            "vdpu_id": "vdpu1",
+            "gnmi_port": "50052"
+        }
+    }
+}
+```
+
+**state**: Administrative status of the DPU (`up` or `down`).
+**vip_ipv4**: VIP IPv4 address from minigraph.
+**vip_ipv6**: VIP IPv6 address from minigraph.
+**pa_ipv4**: PA IPv4 address from minigraph.
+**pa_ipv6**: PA IPv6 address from minigraph.
+**vdpu_id**: ID of VDPUs from minigraph.
+**gnmi_port**: Port gNMI runs on.
 
 # For Developers
 
--- src/sonic-yang-models/tests/yang_model_pytests/test_smart_switch.py
+++ src/sonic-yang-models/tests/yang_model_pytests/test_smart_switch.py
@@ -108,3 +108,165 @@ class TestSmartSwitch:
         }
 
         yang_model.load_data(data, error_message)
+
+    @pytest.mark.parametrize(
+        "port_name, error_message", [
+            ("dpu0", None),
+            ("dp0rt0", 'Value "dp0rt0" does not satisfy the constraint "[a-zA-Z]+[0-9]+"')]
+        )
+    def test_dpu_port_name(self, yang_model, port_name, error_message):
+        data = {
+            "sonic-smart-switch:sonic-smart-switch": {
+                "sonic-smart-switch:DPU_PORT": {
+                    "DPU_PORT_LIST": [
+                        {
+                            "PORT_NAME": port_name,
+                            "state": "up",
+                            "vip_ipv4": "192.168.1.1",
+                            "vip_ipv6": "2001:db8::1",
+                            "pa_ipv4": "192.168.1.2",
+                            "pa_ipv6": "2001:db8::2",
+                            "vdpu_id": "vdpu0",
+                            "gnmi_port": 8080
+                        }
+                    ]
+                }
+            }
+        }
+
+        yang_model.load_data(data, error_message)
+
+    @pytest.mark.parametrize(
+        "vip_ipv4, error_message", [
+            ("192.168.1.1", None),
+            ("192.168.1.xyz", 'Value "192.168.1.xyz" does not satisfy the constraint')]
+        )
+    def test_dpu_port_vip_ipv4(self, yang_model, vip_ipv4, error_message):
+        data = {
+            "sonic-smart-switch:sonic-smart-switch": {
+                "sonic-smart-switch:DPU_PORT": {
+                    "DPU_PORT_LIST": [
+                        {
+                            "PORT_NAME": "dpu0",
+                            "state": "up",
+                            "vip_ipv4": vip_ipv4,
+                            "vip_ipv6": "2001:db8::1",
+                            "pa_ipv4": "192.168.1.2",
+                            "pa_ipv6": "2001:db8::2",
+                            "vdpu_id": "vdpu0",
+                            "gnmi_port": 8080
+                        }
+                    ]
+                }
+            }
+        }
+
+        yang_model.load_data(data, error_message)
+
+    @pytest.mark.parametrize(
+        "vip_ipv6, error_message", [
+            ("2001:db8::1", None),
+            ("2001:db8::xyz", 'Value "2001:db8::xyz" does not satisfy the constraint')]
+        )
+    def test_dpu_port_vip_ipv6(self, yang_model, vip_ipv6, error_message):
+        data = {
+            "sonic-smart-switch:sonic-smart-switch": {
+                "sonic-smart-switch:DPU_PORT": {
+                    "DPU_PORT_LIST": [
+                        {
+                            "PORT_NAME": "dpu0",
+                            "state": "up",
+                            "vip_ipv4": "192.168.1.1",
+                            "vip_ipv6": vip_ipv6,
+                            "pa_ipv4": "192.168.1.2",
+                            "pa_ipv6": "2001:db8::2",
+                            "vdpu_id": "vdpu0",
+                            "gnmi_port": 8080
+                        }
+                    ]
+                }
+            }
+        }
+
+        yang_model.load_data(data, error_message)
+
+    @pytest.mark.parametrize(
+        "pa_ipv4, error_message", [
+            ("192.168.1.2", None),
+            ("192.168.1.xyz", 'Value "192.168.1.xyz" does not satisfy the constraint')]
+        )
+    def test_dpu_port_pa_ipv4(self, yang_model, pa_ipv4, error_message):
+        data = {
+            "sonic-smart-switch:sonic-smart-switch": {
+                "sonic-smart-switch:DPU_PORT": {
+                    "DPU_PORT_LIST": [
+                        {
+                            "PORT_NAME": "dpu0",
+                            "state": "up",
+                            "vip_ipv4": "192.168.1.1",
+                            "vip_ipv6": "2001:db8::1",
+                            "pa_ipv4": pa_ipv4,
+                            "pa_ipv6": "2001:db8::2",
+                            "vdpu_id": "vdpu0",
+                            "gnmi_port": 8080
+                        }
+                    ]
+                }
+            }
+        }
+
+        yang_model.load_data(data, error_message)
+
+    @pytest.mark.parametrize(
+        "pa_ipv6, error_message", [
+            ("2001:db8::2", None),
+            ("2001:db8::xyz", 'Value "2001:db8::xyz" does not satisfy the constraint')]
+        )
+    def test_dpu_port_pa_ipv6(self, yang_model, pa_ipv6, error_message):
+        data = {
+            "sonic-smart-switch:sonic-smart-switch": {
+                "sonic-smart-switch:DPU_PORT": {
+                    "DPU_PORT_LIST": [
+                        {
+                            "PORT_NAME": "dpu0",
+                            "state": "up",
+                            "vip_ipv4": "192.168.1.1",
+                            "vip_ipv6": "2001:db8::1",
+                            "pa_ipv4": "192.168.1.2",
+                            "pa_ipv6": pa_ipv6,
+                            "vdpu_id": "vdpu0",
+                            "gnmi_port": 8080
+                        }
+                    ]
+                }
+            }
+        }
+
+        yang_model.load_data(data, error_message)
+
+    @pytest.mark.parametrize(
+        "gnmi_port, error_message", [
+            (8080, None),
+            (99999, 'Invalid value "99999" in "gnmi_port" element.')]
+        )
+    def test_dpu_port_gnmi(self, yang_model, gnmi_port, error_message):
+        data = {
+            "sonic-smart-switch:sonic-smart-switch": {
+                "sonic-smart-switch:DPU_PORT": {
+                    "DPU_PORT_LIST": [
+                        {
+                            "PORT_NAME": "dpu0",
+                            "state": "up",
+                            "vip_ipv4": "192.168.1.1",
+                            "vip_ipv6": "2001:db8::1",
+                            "pa_ipv4": "192.168.1.2",
+                            "pa_ipv6": "2001:db8::2",
+                            "vdpu_id": "vdpu0",
+                            "gnmi_port": gnmi_port
+                        }
+                    ]
+                }
+            }
+        }
+
+        yang_model.load_data(data, error_message)
