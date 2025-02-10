ba9b096d2244c79e0c70367617069fa4c82c2004
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2025-02-11 07:13:41 +0800

    Improve existing core_cleanup.py to clean old dump/kernel dump.  (#21532)
    Improve existing core_cleanup.py to clean old dump/kernel dump. 
    
    #### Why I did it
    On small disk device, dump file will cause disk full issue.
    
    #### How I did it
    Delete expired dump files
    Only keep 4 kernel dump files.
    
    #### How to verify it
    Pass all UT.
    
    Add new UT to cover this script.
    
    Manually verified:
    
    
    admin@vlab-01:~$ sudo ls /var/dump/
    sonic_dump_bjw-can-7260-8_20230125_030042.tar.gz  sonic_dump_bjw-can-7260-8_20250125_030043.tar.gz  sonic_dump_bjw-can-7260-8_20250125_030045.tar.gz
    sonic_dump_bjw-can-7260-8_20250125_030042.tar.gz  sonic_dump_bjw-can-7260-8_20250125_030044.tar.gz  sonic_dump_bjw-can-7260-8_20250125_030046.tar.gz
    admin@vlab-01:~$ sudo ls /var/core/
    orchagent.1644957238.46.core.gz  orchagent.1944957232.46.core.gz  orchagent.1944957234.46.core.gz  orchagent.1944957236.46.core.gz
    orchagent.1944957231.46.core.gz  orchagent.1944957233.46.core.gz  orchagent.1944957235.46.core.gz
    
    admin@vlab-01:~$ sudo python3 ./core_cleanup.py
    
    admin@vlab-01:~$ sudo ls /var/dump/
    sonic_dump_bjw-can-7260-8_20250125_030043.tar.gz  sonic_dump_bjw-can-7260-8_20250125_030045.tar.gz
    sonic_dump_bjw-can-7260-8_20250125_030044.tar.gz  sonic_dump_bjw-can-7260-8_20250125_030046.tar.gz
    admin@vlab-01:~$ sudo ls /var/core/
    orchagent.1944957233.46.core.gz  orchagent.1944957234.46.core.gz  orchagent.1944957235.46.core.gz  orchagent.1944957236.46.core.gz
    
    #### Description for the changelog
    Improve existing core_cleanup.py to clean old dump/kernel dump.

--- files/scripts/core_cleanup.py
+++ files/scripts/core_cleanup.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python3
 
 import os
+import re
 from collections import defaultdict
-from datetime import datetime
+from datetime import datetime, date, timedelta
 
 from sonic_py_common.logger import Logger
 
 SYSLOG_IDENTIFIER = 'core_cleanup.py'
 CORE_FILE_DIR = '/var/core/'
+KERNEL_DUMP_DIR = '/var/dump/'
 MAX_CORE_FILES = 4
 
+def delete_file(file_path):
+    try:
+        os.remove(file_path)
+    except e:
+        logger.log_error('Unexpected error: {} occured trying to delete {}'.format(e, file_path))
+
+def get_dump_timestamp(file_name):
+    match = re.search(r'sonic_dump_.*_(\d\d\d\d)(\d\d)(\d\d)_(\d\d)(\d\d)(\d\d).tar.gz', file_name)
+    if match:
+        groups = match.groups()
+        return datetime(int(groups[0]), int(groups[1]), int(groups[2]), int(groups[3]), int(groups[4]), int(groups[5]))
+    
+    return None
 
 def main():
     logger = Logger(SYSLOG_IDENTIFIER)
@@ -32,14 +47,32 @@ def main():
             curr_files.sort(reverse=True, key=lambda x: datetime.utcfromtimestamp(int(x.split('.')[1])))
             oldest_core = curr_files[MAX_CORE_FILES]
             logger.log_info('Deleting {}'.format(oldest_core))
-            try:
-                os.remove(os.path.join(CORE_FILE_DIR, oldest_core))
-            except:
-                logger.log_error('Unexpected error occured trying to delete {}'.format(oldest_core))
+            delete_file(os.path.join(CORE_FILE_DIR, oldest_core))
             core_files_by_process[process] = curr_files[0:MAX_CORE_FILES]
 
     logger.log_info('Finished cleaning up core files')
 
+    # cleanup kernel dumps
+    logger.log_info('Cleaning up kernel dump files')
+    kernel_dumps = [f for f in os.listdir(KERNEL_DUMP_DIR) if os.path.isfile(os.path.join(KERNEL_DUMP_DIR, f))]
+    # Sort kernel dumps from new to old
+    kernel_dumps.sort(reverse = True, key = lambda x: get_dump_timestamp(x))
+    not_expired_dumps = []
+    for kernel_dump in kernel_dumps:
+        # delete expired kernel dump
+        dump_date = get_dump_timestamp(kernel_dump)
+        if not dump_date:
+            # Not a kernel dump file
+            continue
+
+        # Only keep recent MAX_CORE_FILES kernel dumps
+        if len(not_expired_dumps) < MAX_CORE_FILES:
+            not_expired_dumps.append(kernel_dump)
+        else:
+            logger.log_info('Deleting {}'.format(kernel_dump))
+            delete_file(os.path.join(KERNEL_DUMP_DIR, kernel_dump))
+
+    logger.log_info('Finished cleaning up kernel dump files')
 
 if __name__ == '__main__':
     main()
