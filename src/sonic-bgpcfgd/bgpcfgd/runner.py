9be3eb59e90f9e90f1531369cd8cc97cd7bddf10
Tejaswini Chadaga <85581939+tjchadaga@users.noreply.github.com>
2025-02-24 11:39:56 -0800

    Revert unix socket for chassis db (#21606)
    Why I did it
    Reverting part of #21345, since Chassis DB connection from remote host (linecard) cannot use unix socket
    
    How I did it
    Continue to use TCP socket for chassis DB connection
    
    How to verify it
    Bring up BGP on chassis, which currently fails after change in #21345

--- src/sonic-bgpcfgd/bgpcfgd/runner.py
+++ src/sonic-bgpcfgd/bgpcfgd/runner.py
@@ -40,7 +40,7 @@ class Runner(object):
         db = swsscommon.SonicDBConfig.getDbId(db_name)
         if db not in self.db_connectors:
             if db_name == "CHASSIS_APP_DB":
-                self.db_connectors[db] = swsscommon.DBConnector(db_name, 0, False, '')
+                self.db_connectors[db] = swsscommon.DBConnector(db_name, 0, True, '')
             else:
                 self.db_connectors[db] = swsscommon.DBConnector(db_name, 0)
 
