b9f793b84c91545d23d4509ed61739d92ac9220d
Saikrishna Arcot <sarcot@microsoft.com>
2025-04-04 08:25:04 -0700

    Upgrade the gbsyncd-broncos and gbsyncd-credo dockers to Bookworm (#22007)
    * Upgrade the gbsyncd-broncos and gbsyncd-credo dockers to Bookworm
    
    Signed-off-by: Saikrishna Arcot <sarcot@microsoft.com>

--- platform/components/docker-gbsyncd-credo.mk
+++ platform/components/docker-gbsyncd-credo.mk
@@ -8,7 +8,7 @@ LIBSAI_CREDO_BLACKHAWK = libsaicredo-blackhawk_0.9.9_amd64.deb
 $(LIBSAI_CREDO_BLACKHAWK)_URL = "https://sonicstorage.blob.core.windows.net/public/credosai/$(LIBSAI_CREDO_BLACKHAWK)"
 
 ifneq ($($(LIBSAI_CREDO)_URL),)
-include $(PLATFORM_PATH)/../template/docker-gbsyncd-base.mk
+include $(PLATFORM_PATH)/../template/docker-gbsyncd-bookworm.mk
 $(DOCKER_GBSYNCD_BASE)_VERSION = 1.0.0
 $(DOCKER_GBSYNCD_BASE)_PACKAGE_NAME = gbsyncd
 $(DOCKER_GBSYNCD_BASE)_PATH = $(PLATFORM_PATH)/../components/docker-gbsyncd-$(DOCKER_GBSYNCD_PLATFORM_CODE)
