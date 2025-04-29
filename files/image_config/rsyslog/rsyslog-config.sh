9b79ceff5ea594d09675f1a0a307085c082f33ab
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2025-04-30 04:25:24 +0800

    Add OS version to rsyslog template. (#22111)
    Add OS version to rsyslog template.
    
    Why I did it
    Add OS version information to rsyslog will help investigation SONiC issue happened during OS upgrade.
    
    Work item tracking
    Microsoft ADO (number only): 31890431
    How I did it
    Add OS version to rsyslog template
    Make this feature CONFIG_DB flag: DEVICE_METADATA table 'syslog_with_osversion' field
    A hostcfgd PR to restart rsyslog-config service accotding to config change:
    sonic-net/sonic-host-services#245
    
    How to verify it
    Pass all test case.
    Manually verified the syslog contains version info after enable it.

--- files/image_config/rsyslog/rsyslog-config.sh
+++ files/image_config/rsyslog/rsyslog-config.sh
@@ -25,8 +25,18 @@ fi
 
 hostname=$(hostname)
 
+syslog_with_osversion=$(sonic-db-cli CONFIG_DB hget "DEVICE_METADATA|localhost" "syslog_with_osversion")
+if [ -z "$syslog_with_osversion" ]; then
+    syslog_with_osversion="false"
+fi
+
+os_version=$(sonic-cfggen -y /etc/sonic/sonic_version.yml -v build_version)
+if [ -z "$os_version" ]; then
+    os_version="Unknown"
+fi
+
 sonic-cfggen -d -t /usr/share/sonic/templates/rsyslog.conf.j2 \
-    -a "{\"udp_server_ip\": \"$udp_server_ip\", \"hostname\": \"$hostname\", \"docker0_ip\": \"$docker0_ip\"}" \
+    -a "{\"udp_server_ip\": \"$udp_server_ip\", \"hostname\": \"$hostname\", \"docker0_ip\": \"$docker0_ip\", \"forward_with_osversion\": \"$syslog_with_osversion\", \"os_version\": \"$os_version\"}" \
     > /etc/rsyslog.conf
 
 systemctl restart rsyslog
