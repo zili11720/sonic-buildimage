b16598375a59cfaab1f1e0d24da9e0e1843320e6
prabhataravind <108555774+prabhataravind@users.noreply.github.com>
2025-05-07 09:33:34 -0700

    Enable ZMQ between orchagent and syncd for smartswitch DPUs (#21940)
    Enable ZMQ between orchagent and syncd for smartswitch DPUs

--- dockers/docker-orchagent/orchagent.sh
+++ dockers/docker-orchagent/orchagent.sh
@@ -28,9 +28,13 @@ else
     ORCHAGENT_ARGS+="-b 1024 "
 fi
 
-# Set synchronous mode if it is enabled in CONFIG_DB
+# Set zmq mode by default for smartswitch DPU
+# Otherwise, set synchronous mode if it is enabled in CONFIG_DB
 SYNC_MODE=$(echo $SWSS_VARS | jq -r '.synchronous_mode')
-if [ "$SYNC_MODE" == "enable" ]; then
+SWITCH_TYPE=$(echo $SWSS_VARS | jq -r '.switch_type')
+if [ "$SWITCH_TYPE" == "dpu" ]; then
+    ORCHAGENT_ARGS+="-z zmq_sync "
+elif [ "$SYNC_MODE" == "enable" ]; then
     ORCHAGENT_ARGS+="-s "
 fi
 
