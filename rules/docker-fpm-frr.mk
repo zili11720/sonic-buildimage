73de86fbaf4adc5dcab9aacc2a1a3c36c70f48f1
rameshraghupathy <43161235+rameshraghupathy@users.noreply.github.com>
2025-05-13 21:01:30 -0700

    Including platform_utils in make and removing default initialization of $SMARTSWITCH in TSA, TSB, TSC (#22518)
    Including platform_utils in make and removing default initialization of $SMARTSWITCH in TSA, TSB, TSC

--- rules/docker-fpm-frr.mk
+++ rules/docker-fpm-frr.mk
@@ -40,6 +40,7 @@ $(DOCKER_FPM_FRR)_BASE_IMAGE_FILES += TSA:/usr/bin/TSA
 $(DOCKER_FPM_FRR)_BASE_IMAGE_FILES += TSB:/usr/bin/TSB
 $(DOCKER_FPM_FRR)_BASE_IMAGE_FILES += TSC:/usr/bin/TSC
 $(DOCKER_FPM_FRR)_BASE_IMAGE_FILES += TS:/usr/bin/TS
+$(DOCKER_FPM_FRR)_BASE_IMAGE_FILES += platform_utils:/usr/bin/platform_utils
 $(DOCKER_FPM_FRR)_BASE_IMAGE_FILES += idf_isolation:/usr/bin/idf_isolation
 $(DOCKER_FPM_FRR)_BASE_IMAGE_FILES += prefix_list:/usr/bin/prefix_list
 
