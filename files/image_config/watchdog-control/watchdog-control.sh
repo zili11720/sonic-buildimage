ff9e136cac5c69c86cd94b09b3d6bc5974e76b3c
Vasundhara Volam <163894573+vvolam@users.noreply.github.com>
2025-05-21 15:54:25 -0700

    [smartswitch] Enable watchdog utility for DPUs (#22315)
    * Do not disable watchdog on DPU
    
    * Address review comments

--- files/image_config/watchdog-control/watchdog-control.sh
+++ files/image_config/watchdog-control/watchdog-control.sh
@@ -3,6 +3,11 @@
 VERBOSE=no
 WATCHDOG_UTIL="/usr/local/bin/watchdogutil"
 
+# read SONiC immutable variables
+[ -f /etc/sonic/sonic-environment ] && . /etc/sonic/sonic-environment
+PLATFORM=${PLATFORM:-$(sonic-db-cli CONFIG_DB HGET 'DEVICE_METADATA|localhost' platform)}
+PLATFORM_JSON="/usr/share/sonic/device/$PLATFORM/platform.json"
+
 function debug()
 {
     /usr/bin/logger "$0 : $1"
@@ -34,6 +39,19 @@ function getBootType()
     echo "${TYPE}"
 }
 
+function is_smart_switch_dpu()
+{
+    if [[ ! -f "$PLATFORM_JSON" ]]; then
+        return 1
+    fi
+
+    if jq -e 'has("DPU")' "$PLATFORM_JSON" > /dev/null; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 function disable_watchdog()
 {
     # Obtain boot type from kernel arguments
@@ -44,4 +62,20 @@ function disable_watchdog()
     fi
 }
 
-disable_watchdog
+function enable_watchdog()
+{
+    if [[ -x ${WATCHDOG_UTIL} ]]; then
+        debug "Enabling Watchdog"
+        ${WATCHDOG_UTIL} arm
+    fi
+}
+
+if is_smart_switch_dpu; then
+    # Keep watchdog enabled for smart switch DPUs
+    # Smart switch DPUs uses ARM SBSA Generic Watchdog,
+    # which is capable of monitoring the system in runtime.
+    enable_watchdog
+else
+    # Disable watchdog for all other platforms
+    disable_watchdog
+fi
