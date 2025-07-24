dde584dd89995664283ffe92a0ef2e7f11d00eb9
Oleksandr Ivantsiv <oivantsiv@nvidia.com>
2025-07-24 10:59:40 -0700

    [DPU] Update SAI to SAIBuild0.0.44.0, FW to v45.1020 (#23356)
     [DPU] Update SAI to SAIBuild0.0.44.0, FW to v45.1020

--- platform/nvidia-bluefield/recipes/sdk.mk
+++ platform/nvidia-bluefield/recipes/sdk.mk
@@ -19,7 +19,7 @@ SDK_BASE_PATH = $(PLATFORM_PATH)/sdk-src/sonic-bluefield-packages/bin
 
 # Place here URL where SDK sources exist
 SDK_SOURCE_BASE_URL =
-SDK_VERSION = 25.7-RC1
+SDK_VERSION = 25.7-RC3
 
 SDK_COLLECTX_URL = https://linux.mellanox.com/public/repo/doca/1.5.2/debian12/aarch64/
 
