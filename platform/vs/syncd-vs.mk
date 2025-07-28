25e9135481c3dcc997211922934ad9ca9698efe2
Yakiv Huryk <62013282+Yakiv-Huryk@users.noreply.github.com>
2025-07-28 21:40:43 +0300

    build: add INCLUDE_VS_DASH_SAI flag to make dash-sai compilation optional (#23122)
    build: add INCLUDE_VS_DASH_SAI flag to make dash-sai compilation optional

--- platform/vs/syncd-vs.mk
+++ platform/vs/syncd-vs.mk
@@ -3,6 +3,7 @@ $(LIBSAIREDIS)_DEB_BUILD_PROFILES += syncd vs
 SYNCD_VS = syncd-vs_1.0.0_$(CONFIGURED_ARCH).deb
 $(SYNCD_VS)_RDEPENDS += $(LIBSAIREDIS) $(LIBSAIMETADATA) $(LIBSAIVS)
 
+ifeq ($(INCLUDE_VS_DASH_SAI),y)
 ifeq ($(BLDENV),bookworm)
 # dash-sai only support sonic-vs.img.gz. it don't support docker-sonic-vs.gz
 ifeq ($(findstring docker-sonic-vs, $(SONIC_BUILD_TARGET)), )
@@ -13,7 +14,7 @@ endif
 else
   $(warning DASH_SAI cannot support this build environment $(BLDENV))
 endif
-
+endif
 
 $(eval $(call add_derived_package,$(LIBSAIREDIS),$(SYNCD_VS)))
 
