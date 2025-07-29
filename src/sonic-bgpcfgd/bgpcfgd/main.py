18540e46b715cba14b0f06f21ff99de3d6be856a
Yaqiang Zhu <zyq1512099831@gmail.com>
2025-07-30 00:16:32 +0800

    [T2] Update role check in bgpcfgd for SpineRouter roles (#23498)
    Why I did it
    Align bgpcfgd for SpineRouter with this PR: https://github.com/sonic-net/sonic-buildimage/pull/22337/files#diff-f2a60bd6a3c68f7e519d112b6ef206e57a063ffbe0bdbc47ad36d2516fb8669d
    
    How I did it
    Start PrefixListMgr and AsPathMgr for UpperSpineRouter
    
    How to verify it
    Manually test
    
    admin@sonic:~$ sonic-db-cli CONFIG_DB hset "DEVICE_METADATA|localhost" "type" "UpperSpineRouter"
    0
    admin@sonic:~$ sudo tail -f /var/log/syslog | grep bgpcfgd | grep Manager
    2025 Jul 28 05:40:17.400381 sonic NOTICE bgp#bgpcfgd: Prefix List Manager and AsPath Manager are enabled for UpperSpineRouter/UpstreamLC

--- src/sonic-bgpcfgd/bgpcfgd/main.py
+++ src/sonic-bgpcfgd/bgpcfgd/main.py
@@ -96,10 +96,16 @@ def do_work():
         managers.append(BfdMgr(common_objs, "STATE_DB", swsscommon.STATE_BFD_SOFTWARE_SESSION_TABLE_NAME))
 
     device_metadata = config_db.get_table("DEVICE_METADATA")
-    if "localhost" in device_metadata and "type" in device_metadata["localhost"] and device_metadata["localhost"]["type"] == "SpineRouter" and "subtype" in device_metadata["localhost"] and device_metadata["localhost"]["subtype"] == "UpstreamLC":
+    # Enable Prefix List Manager and AsPath Manager for UpperSpineRouter/UpstreamLC
+    is_upstream_lc = ("localhost" in device_metadata and "type" in device_metadata["localhost"] and "subtype" in device_metadata["localhost"] and
+                      device_metadata["localhost"]["type"] == "SpineRouter" and device_metadata["localhost"]["subtype"] == "UpstreamLC")
+    is_upper_spine_router = ("localhost" in device_metadata and "type" in device_metadata["localhost"] and
+                             device_metadata["localhost"]["type"] == "UpperSpineRouter")
+    if is_upstream_lc or is_upper_spine_router:
         # Prefix List Manager
         managers.append(PrefixListMgr(common_objs, "CONFIG_DB", "PREFIX_LIST"))
         managers.append(AsPathMgr(common_objs, "CONFIG_DB", "DEVICE_METADATA"))
+        log_notice("Prefix List Manager and AsPath Manager are enabled for UpperSpineRouter/UpstreamLC")
 
     runner = Runner(common_objs['cfg_mgr'])
     for mgr in managers:
