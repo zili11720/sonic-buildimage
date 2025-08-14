61ceb1a695ab2c0eaa7fa8911a6677beb1b89e1b
Marty Y. Lok <76118573+mlok-nokia@users.noreply.github.com>
2025-08-14 11:30:26 -0400

    [warm reboot] Using sonic-db-cli to optimize DB access time for warm reboot to come up on time (#22628)
    With sonic-cfggen, it introduces more time for system to come up during wram reboot. This will cause LACP session terminated. This PR modified the code to use sonic-db-cli instead of sonic-cfggen to optimize the DB base access to reduce the bootup time. It fixes issue #22438
    
    ---------
    
    Signed-off-by: mlok <marty.lok@nokia.com>

--- files/image_config/chrony/chronyd-starter.sh
+++ files/image_config/chrony/chronyd-starter.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-VRF_ENABLED=$(/usr/local/bin/sonic-cfggen -d -v 'MGMT_VRF_CONFIG["vrf_global"]["mgmtVrfEnabled"]' 2> /dev/null)
+VRF_ENABLED=$(sonic-db-cli CONFIG_DB HGET "MGMT_VRF_CONFIG|vrf_global" "mgmtVrfEnabled" 2> /dev/null)
 if [ "$VRF_ENABLED" = "true" ]; then
-    VRF_CONFIGURED=$(/usr/local/bin/sonic-cfggen -d -v 'NTP["global"]["vrf"]' 2> /dev/null)
+    VRF_CONFIGURED=$(sonic-db-cli CONFIG_DB HGET "NTP|global" "vrf" 2> /dev/null)
     if [ "$VRF_CONFIGURED" = "default" ]; then
         echo "Starting NTP server in default-vrf for default set as NTP vrf"
         exec /usr/sbin/chronyd $DAEMON_OPTS
--- files/scripts/asic_status.sh
+++ files/scripts/asic_status.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-PLATFORM="$(sonic-cfggen -H -v DEVICE_METADATA.localhost.platform)"
-PLATFORM_ENV_CONF=/usr/share/sonic/device/$PLATFORM/platform_env.conf
-[ -f $PLATFORM_ENV_CONF ] && . $PLATFORM_ENV_CONF
-
 is_chassis_supervisor() {
-    if [ -f /etc/sonic/chassisdb.conf ] && [[ $disaggregated_chassis -ne 1 ]]; then
-        true
-        return
+    if [ -f /etc/sonic/chassisdb.conf ]; then
+        PLATFORM=$(sonic-db-cli CONFIG_DB HGET "DEVICE_METADATA|localhost" "platform")
+        PLATFORM_ENV_CONF=/usr/share/sonic/device/$PLATFORM/platform_env.conf
+        [ -f $PLATFORM_ENV_CONF ] && . $PLATFORM_ENV_CONF
+        if [[ $disaggregated_chassis -ne 1 ]]; then
+            true
+            return
+        fi
     fi
     false
     return
