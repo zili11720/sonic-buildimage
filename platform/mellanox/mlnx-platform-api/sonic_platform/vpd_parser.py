eb950f942ead01d0ddca8c17df565c61f02b3b42
Zili Bombach <zbombach@r-build-sonic05.mtr.labs.mlnx>
2025-09-07 12:47:23 +0300

    vpd_parser: added debug logs for PSU VPD parsing
    Signed-off-by: Zili Bombach <zbombach@r-build-sonic05.mtr.labs.mlnx>

--- platform/mellanox/mlnx-platform-api/sonic_platform/vpd_parser.py
+++ platform/mellanox/mlnx-platform-api/sonic_platform/vpd_parser.py
@@ -57,6 +57,8 @@ class VpdParser:
         """
         if self._get_data() and PN_VPD_FIELD not in self.vpd_data:
             logger.log_error("Fail to read model number: No key {} in VPD {}".format(PN_VPD_FIELD, self.vpd_file))
+            # Log the entire VPD data for debugging
+            logger.log_debug("VPD data content for {}: {}".format(self.vpd_file, self.vpd_data))
             return 'N/A'
         return self.vpd_data.get(PN_VPD_FIELD, 'N/A')
 
@@ -69,6 +71,8 @@ class VpdParser:
         """
         if self._get_data() and SN_VPD_FIELD not in self.vpd_data:
             logger.log_error("Fail to read serial number: No key {} in VPD {}".format(SN_VPD_FIELD, self.vpd_file))
+            # Log the entire VPD data for debugging
+            logger.log_debug("VPD data content for {}: {}".format(self.vpd_file, self.vpd_data))
             return 'N/A'
         return self.vpd_data.get(SN_VPD_FIELD, 'N/A')
 
@@ -81,6 +85,8 @@ class VpdParser:
         """
         if self._get_data() and REV_VPD_FIELD not in self.vpd_data:
             logger.log_error("Fail to read revision: No key {} in VPD {}".format(REV_VPD_FIELD, self.vpd_file))
+            # Log the entire VPD data for debugging
+            logger.log_debug("VPD data content for {}: {}".format(self.vpd_file, self.vpd_data))
             return 'N/A'
         return self.vpd_data.get(REV_VPD_FIELD, 'N/A')
 
