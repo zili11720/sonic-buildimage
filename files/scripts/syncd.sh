46eafcbb00ae7c2d4295b598ffc3d0303640480b
Volodymyr Samotiy <volodymyrs@nvidia.com>
2025-09-22 20:15:40 +0300

    [Mellanox] Stop SDK health monitor before syncd shutdown (#23681)
    [Mellanox] Stop SDK health monitor before syncd shutdown

--- files/scripts/syncd.sh
+++ files/scripts/syncd.sh
@@ -96,6 +96,10 @@ function stopplatform1() {
         debug "Stopped pmon service"
     fi
 
+    if [[ x$sonic_asic_platform == x"mellanox" ]]; then
+        echo "health_check_trigger del_dev 1" > /proc/mlx_sx/sx_core
+    fi
+
     if [[ x$sonic_asic_platform != x"mellanox" ]] || [[ x$TYPE != x"cold" ]]; then
         # Invoke platform specific pre shutdown routine.
         PLATFORM=`$SONIC_DB_CLI CONFIG_DB hget 'DEVICE_METADATA|localhost' platform`
