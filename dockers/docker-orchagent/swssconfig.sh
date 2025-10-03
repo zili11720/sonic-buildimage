81fb5417a581e53b79ae983b7a7e1d7da8ecfb96
Dev Ojha <47282568+developfast@users.noreply.github.com>
2025-10-03 10:07:08 -0700

    [swss][warm-boot] Always load TUNNEL_DECAP config during warm boot if it exists except on broadcom ASICs (#24162)
    [swss][warm-boot] Always load TUNNEL_DECAP config during warm boot if it exists except on broadcom ASICs

--- dockers/docker-orchagent/swssconfig.sh
+++ dockers/docker-orchagent/swssconfig.sh
@@ -49,13 +49,48 @@ fast_reboot
 [ -f /etc/sonic/sonic-environment ] && . /etc/sonic/sonic-environment
 
 HWSKU=${HWSKU:-`sonic-cfggen -d -v "DEVICE_METADATA['localhost']['hwsku']"`}
+sonic_asic_type=$(sonic-cfggen -y /etc/sonic/sonic_version.yml -v asic_type)
+
+# Apply only TUNNEL_DECAP_TABLE entries
+apply_ipinip_subset() {
+  if [[ -f /etc/swss/config.d/ipinip.json ]]; then
+    tmpfile=$(mktemp /tmp/ipinip.XXXX.json)
+
+    jq '[ .[] 
+          | select(
+                has("TUNNEL_DECAP_TABLE:IPINIP_TUNNEL")
+            or has("TUNNEL_DECAP_TABLE:IPINIP_V6_TUNNEL")
+            or has("TUNNEL_DECAP_TABLE:IPINIP_SUBNET")
+            or has("TUNNEL_DECAP_TABLE:IPINIP_SUBNET_V6")
+          )
+        ]' /etc/swss/config.d/ipinip.json > "$tmpfile"
+
+    if [[ -s "$tmpfile" ]]; then
+        echo "Applying filtered ipinip.json subset from $tmpfile"
+        swssconfig "$tmpfile"
+    else
+        echo "No matching TUNNEL_DECAP_TABLE entries found in ipinip.json"
+    fi
+
+    rm -f "$tmpfile"
+    sleep 1
+  fi
+}
 
 # Don't load json config if system warm start or
 # swss docker warm start is enabled, the data already exists in appDB.
 SYSTEM_WARM_START=`sonic-db-cli STATE_DB hget "WARM_RESTART_ENABLE_TABLE|system" enable`
 SWSS_WARM_START=`sonic-db-cli STATE_DB hget "WARM_RESTART_ENABLE_TABLE|swss" enable`
 if [[ "$SYSTEM_WARM_START" == "true" ]] || [[ "$SWSS_WARM_START" == "true" ]]; then
-  exit 0
+    # On warm boot, only apply TUNNEL_DECAP_TABLE subset on non-Broadcom ASICs to
+    # match ipinip.json.j2 config
+    if [[ "$sonic_asic_type" != "broadcom" ]]; then
+        echo "Preparing to apply ipinip.json config for non-broadcom ASIC switch"
+        apply_ipinip_subset
+    else
+        echo "Skip applying ipinip.json config for broadcom ASIC switch after warm-boot"
+    fi
+    exit 0
 fi
 
 SWSSCONFIG_ARGS="ipinip.json ports.json switch.json vxlan.json"
