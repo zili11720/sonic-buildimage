0de865d0714ff40d4182ab46dbe1a683794ef6b5
Linsongnan <linsongnan.lsn@alibaba-inc.com>
2025-10-10 07:26:11 +0800

    [frrcfgd]: remove wrong check of srv6 locator (#23877)
    [frrcfgd]: remove wrong check of srv6 locator

--- src/sonic-frr-mgmt-framework/frrcfgd/frrcfgd.py
+++ src/sonic-frr-mgmt-framework/frrcfgd/frrcfgd.py
@@ -2718,17 +2718,14 @@ class BGPConfigDaemon:
                 else:
                     self.__delete_vrf_asn(vrf, table, data)
             elif table == 'SRV6_MY_LOCATORS':
-                if key is None:
-                    syslog.syslog(syslog.LOG_ERR, 'invalid key for SRV6_MY_LOCATORS table')
-                    continue
                 if not del_table:
-                    key = prefix
+                    locator_name = prefix
                     prefix = data['prefix']
-                    cmd =  "vtysh -c 'configure terminal' -c 'segment-routing' -c 'srv6' -c 'locators' "
-                    cmd += " -c 'locator {}' ".format(key)
+                    cmd = "vtysh -c 'configure terminal' -c 'segment-routing' -c 'srv6' -c 'locators' "
+                    cmd += " -c 'locator {}' ".format(locator_name)
                     cmd += " -c 'prefix {} block-len {} node-len {} func-bits {}' ".format(prefix.data, data['block_len'].data, data['node_len'].data, data['func_len'].data)
                     if not self.__run_command(table, cmd):
-                        syslog.syslog(syslog.LOG_ERR, 'failed running SRV6 POLICY config command')
+                        syslog.syslog(syslog.LOG_ERR, 'failed running SRV6 LOCATORS config command')
                         continue
             elif table == 'SRV6_MY_SOURCE':
                 source = data['source-address']
