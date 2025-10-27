856dac5298923151e8624bb54ee59cc5d8d0e04d
lotus-nexthop <lotus@nexthop.ai>
2025-10-27 16:10:26 -0700

    avoid num_components crash (#24340)

--- platform/pddf/platform-api-pddf-base/sonic_platform_pddf_base/pddf_chassis.py
+++ platform/pddf/platform-api-pddf-base/sonic_platform_pddf_base/pddf_chassis.py
@@ -74,47 +74,47 @@ class PddfChassis(ChassisBase):
 
 
         # FANs
-        for i in range(self.platform_inventory['num_fantrays']):
+        for i in range(self.platform_inventory.get('num_fantrays', 0)):
             fandrawer = FanDrawer(i, self.pddf_obj, self.plugin_data)
             self._fan_drawer_list.append(fandrawer)
             self._fan_list.extend(fandrawer._fan_list)
 
         # PSUs
-        for i in range(self.platform_inventory['num_psus']):
+        for i in range(self.platform_inventory.get('num_psus', 0)):
             psu = Psu(i, self.pddf_obj, self.plugin_data)
             self._psu_list.append(psu)
 
         # OPTICs
-        for index in range(self.platform_inventory['num_ports']):
+        for index in range(self.platform_inventory.get('num_ports', 0)):
             sfp = Sfp(index, self.pddf_obj, self.plugin_data)
             self._sfp_list.append(sfp)
 
         # THERMALs
-        for i in range(self.platform_inventory['num_temps']):
+        for i in range(self.platform_inventory.get('num_temps', 0)):
             thermal = Thermal(i, self.pddf_obj, self.plugin_data)
             self._thermal_list.append(thermal)
 
         if voltage_sensor_present:
             # VOLTAGE SENSORs
-            for i in range(self.platform_inventory['num_voltage_sensors']):
+            for i in range(self.platform_inventory.get('num_voltage_sensors', 0)):
                 voltage = VoltageSensor(i, self.pddf_obj, self.plugin_data)
                 self._voltage_sensor_list.append(voltage)
 
         if current_sensor_present:
             # CURRENT SENSORs
-            for i in range(self.platform_inventory['num_current_sensors']):
+            for i in range(self.platform_inventory.get('num_current_sensors', 0)):
                 current = CurrentSensor(i, self.pddf_obj, self.plugin_data)
                 self._current_sensor_list.append(current)
 
         if asicthermal_present:
             # ASIC Thermal
-            for i in range(self.platform_inventory['num_asic_temps']):
+            for i in range(self.platform_inventory.get('num_asic_temps', 0)):
                 asicthermal = AsicThermal(i, self.pddf_obj)
                 self._thermal_list.append(asicthermal)
 
         if component_present:
             # Components (Programmables)
-            for i in range(self.platform_inventory['num_components']):
+            for i in range(self.platform_inventory.get('num_components', 0)):
                 component = Component(i, self.pddf_obj, self.plugin_data)
                 self._component_list.append(component)
 
