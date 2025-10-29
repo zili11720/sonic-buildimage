3717fb11dfd33826ca8c816aaa3e04867112632d
Stepan Blyshchak <38952541+stepanblyschak@users.noreply.github.com>
2025-10-29 20:27:48 +0200

    [supervisord] fix 1sec delay (#24190)
    Why I did it
    supervisord has 1 sec delay at init. This PR partially addresses #13765
    
    Work item tracking
    Microsoft ADO (number only):
    How I did it
    On init make the polling non blocking in supervisord
    
    How to verify it
    Boot the image, check time between supervisord start and first process start. There should be no 1 sec delay.

---
+++ rules/supervisor.mk
@@ -0,0 +1,6 @@
+# supervisor python3 wheel
+
+SUPERVISOR = supervisor-4.2.5-py2.py3-none-any.whl
+$(SUPERVISOR)_SRC_PATH = $(SRC_PATH)/supervisor
+$(SUPERVISOR)_PYTHON_VERSION = 3
+SONIC_PYTHON_WHEELS += $(SUPERVISOR)
