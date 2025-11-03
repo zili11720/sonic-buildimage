7abc99f2d98bf6ca9242ba4ac22a9844483e4ab7
Saikrishna Arcot <sarcot@microsoft.com>
2025-11-03 09:13:35 -0800

    Update monit to 5.34
    Co-authored-by: Hemanth Kumar Tirupati <tirupatihemanthkumar@gmail.com>
    Signed-off-by: Saikrishna Arcot <sarcot@microsoft.com>

--- src/system-health/health_checker/service_checker.py
+++ src/system-health/health_checker/service_checker.py
@@ -48,13 +48,9 @@ class ServiceChecker(HealthChecker):
     CHECK_CMD = 'monit summary -B'
     MIN_CHECK_CMD_LINES = 3
 
-    # Expect status for different system service category.
-    EXPECT_STATUS_DICT = {
-        'System': 'Running',
-        'Process': 'Running',
-        'Filesystem': 'Accessible',
-        'Program': 'Status ok'
-    }
+    # Expect status for all system service categories.
+    # Monit 5.34.3+ (Debian 13) uses 'OK' for all service types
+    EXPECTED_STATUS = 'OK'
 
     def __init__(self):
         HealthChecker.__init__(self)
@@ -290,11 +286,8 @@ class ServiceChecker(HealthChecker):
                 continue
             status = line[status_begin:type_begin].strip()
             service_type = line[type_begin:].strip()
-            if service_type not in ServiceChecker.EXPECT_STATUS_DICT:
-                continue
-            expect_status = ServiceChecker.EXPECT_STATUS_DICT[service_type]
-            if expect_status != status:
-                self.set_object_not_ok(service_type, name, '{} is not {}'.format(name, expect_status))
+            if status != ServiceChecker.EXPECTED_STATUS:
+                self.set_object_not_ok(service_type, name, '{} status is {}, expected {}'.format(name, status, ServiceChecker.EXPECTED_STATUS))
             else:
                 self.set_object_ok(service_type, name)
         return
