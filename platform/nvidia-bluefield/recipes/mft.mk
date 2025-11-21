78142a96e707c0af817556e321a7484d1b340f7b
Hemanth Kumar Tirupati <tirupatihemanthkumar@gmail.com>
2025-11-21 14:22:11 -0800

    [nvidia-bluefield] Fix OFED compilation failure in latest NASA
    * Dont run dkms build command twice
    
    Signed-off-by: Vivek Reddy <vkarri@nvidia.com>
    
    * Install linux headers before DKMS package
    
    Signed-off-by: Vivek Reddy <vkarri@nvidia.com>
    
    * Add an ability to pass custom options or env variables to install targets
    
    Signed-off-by: Vivek Reddy <vkarri@nvidia.com>
    
    ---------
    
    Signed-off-by: Vivek Reddy <vkarri@nvidia.com>
    Co-authored-by: Vivek Reddy <vkarri@nvidia.com>

--- platform/nvidia-bluefield/recipes/mft.mk
+++ platform/nvidia-bluefield/recipes/mft.mk
@@ -35,10 +35,13 @@ $(eval $(call add_derived_package,$(MFT),$(MFT_OEM)))
 
 KERNEL_MFT_DKMS = kernel-mft-dkms_$(MFT_VERSION)-$(MFT_REVISION)_all.deb
 $(eval $(call add_derived_package,$(MFT),$(KERNEL_MFT_DKMS)))
+$(KERNEL_MFT_DKMS)_DEPENDS = $(LINUX_HEADERS) $(LINUX_HEADERS_COMMON)
+# Note: Restrict the DKMS package to compile the driver only against the target kernel headers
+$(KERNEL_MFT_DKMS)_DEB_INSTALL_OPTS = "KVERSION=$(KVERSION)"
 
 BUILD_ARCH := $(shell dpkg-architecture -qDEB_BUILD_ARCH)
 KERNEL_MFT = kernel-mft-dkms-modules-$(KVERSION)_$(MFT_VERSION)_$(BUILD_ARCH).deb
 $(KERNEL_MFT)_SRC_PATH = $(PLATFORM_PATH)/mft
-$(KERNEL_MFT)_DEPENDS += $(LINUX_HEADERS) $(LINUX_HEADERS_COMMON) $(KERNEL_MFT_DKMS)
+$(KERNEL_MFT)_DEPENDS += $(KERNEL_MFT_DKMS)
 
 SONIC_MAKE_DEBS += $(MFT) $(KERNEL_MFT)
