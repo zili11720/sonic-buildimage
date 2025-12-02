3ee939e8b599f4af4840959a05dce2a75e21d446
Ben Levi <106863159+benle7@users.noreply.github.com>
2025-12-02 17:41:08 +0200

    [Mellanox] Add SAI API headers versions to get_component_versions (#24662)
    - Why I did it
    Improve get_component_versions​ to present the SAI API headers version.
    For debugging and compatibility validation, it is important to know:​
    
    The SAI API version that SONiC was compiled against​.
    The SAI API version implemented by the vendor’s installed SAI library at runtime.
    
    - How I did it
    The SAI API version that SONiC was compiled against:
    During compilation, create_component_versions.sh extracts the version​
    from src/sonic-sairedis/SAI/inc/saiversion.h and save it into temp component-versions file.
    The SAI API version implemented by the vendor’s installed SAI library at runtime:
    During runtime, using syncd:dpkg -s mlnx-sai
    
    - How to verify it
    run: get_component_versions.py
    
    Signed-off-by: Ben Levi <belevi@nvidia.com>

--- platform/mellanox/component-versions/create_component_versions.sh
+++ platform/mellanox/component-versions/create_component_versions.sh
@@ -1,5 +1,6 @@
 #
-# Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES.
+# SPDX-FileCopyrightText: NVIDIA CORPORATION & AFFILIATES
+# Copyright (c) 2024-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,6 +19,21 @@
 echo "SDK $1" > temp_versions_file
 echo $2 | sed -r 's/([0-9]*)\.([0-9]*)\.([0-9]*)/FW \2\.\3/g' >> temp_versions_file
 echo "SAI $3" >> temp_versions_file
+
+SAI_API_VERSION_PATH="../../../src/sonic-sairedis/SAI/inc/saiversion.h"
+if [ -f "$SAI_API_VERSION_PATH" ]; then
+    SAI_MAJOR=$(grep "SAI_MAJOR" "$SAI_API_VERSION_PATH" | grep -oE "[0-9]+")
+    SAI_MINOR=$(grep "SAI_MINOR" "$SAI_API_VERSION_PATH" | grep -oE "[0-9]+")
+    SAI_REVISION=$(grep "SAI_REVISION" "$SAI_API_VERSION_PATH" | grep -oE "[0-9]+")
+    if [ -n "$SAI_MAJOR" ] && [ -n "$SAI_MINOR" ] && [ -n "$SAI_REVISION" ]; then
+        echo "SAI_API_HEADERS $SAI_MAJOR.$SAI_MINOR.$SAI_REVISION" >> temp_versions_file
+    else
+        echo "SAI_API_HEADERS N/A" >> temp_versions_file
+    fi
+else
+    echo "SAI_API_HEADERS N/A" >> temp_versions_file
+fi
+
 echo "HW_MANAGEMENT $4" >> temp_versions_file
 echo "MFT $5-$6" >> temp_versions_file
 echo "KERNEL $7" >> temp_versions_file
