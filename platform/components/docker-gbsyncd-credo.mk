43eb6e2cc8714c8156c3486c273e119372e8c8cc
Tejaswini Chadaga <85581939+tjchadaga@users.noreply.github.com>
2025-12-12 09:44:48 -0800

    Update libsaicredo package versions and dependencies (#24762)
    Signed-off-by: Tejaswini Chadaga <tchadaga@microsoft.com>

--- platform/components/docker-gbsyncd-credo.mk
+++ platform/components/docker-gbsyncd-credo.mk
@@ -1,17 +1,19 @@
 DOCKER_GBSYNCD_PLATFORM_CODE = credo
 
-LIBSAI_CREDO = libsaicredo_0.9.9_amd64.deb
+LIBSAI_CREDO = libsaicredo_1.2.4_amd64.deb
 $(LIBSAI_CREDO)_URL = "https://packages.trafficmanager.net/public/credosai/$(LIBSAI_CREDO)"
-LIBSAI_CREDO_OWL = libsaicredo-owl_0.9.9_amd64.deb
+LIBSAI_CREDO_OWL = libsaicredo-owl_1.2.4_amd64.deb
 $(LIBSAI_CREDO_OWL)_URL = "https://packages.trafficmanager.net/public/credosai/$(LIBSAI_CREDO_OWL)"
-LIBSAI_CREDO_BLACKHAWK = libsaicredo-blackhawk_0.9.9_amd64.deb
+LIBSAI_CREDO_BLACKHAWK = libsaicredo-blackhawk_1.2.4_amd64.deb
 $(LIBSAI_CREDO_BLACKHAWK)_URL = "https://packages.trafficmanager.net/public/credosai/$(LIBSAI_CREDO_BLACKHAWK)"
+LIBSAI_CREDO_CRT88322 = libsaicredo-crt88322_1.2.4_amd64.deb
+$(LIBSAI_CREDO_CRT88322)_URL = "https://packages.trafficmanager.net/public/credosai/$(LIBSAI_CREDO_CRT88322)"
 
 ifneq ($($(LIBSAI_CREDO)_URL),)
 include $(PLATFORM_PATH)/../template/docker-gbsyncd-bookworm.mk
 $(DOCKER_GBSYNCD_BASE)_VERSION = 1.0.0
 $(DOCKER_GBSYNCD_BASE)_PACKAGE_NAME = gbsyncd
 $(DOCKER_GBSYNCD_BASE)_PATH = $(PLATFORM_PATH)/../components/docker-gbsyncd-$(DOCKER_GBSYNCD_PLATFORM_CODE)
-SONIC_ONLINE_DEBS += $(LIBSAI_CREDO) $(LIBSAI_CREDO_OWL) $(LIBSAI_CREDO_BLACKHAWK)
-$(DOCKER_GBSYNCD_BASE)_DEPENDS += $(SYNCD) $(LIBSAI_CREDO) $(LIBSAI_CREDO_OWL) $(LIBSAI_CREDO_BLACKHAWK)
+SONIC_ONLINE_DEBS += $(LIBSAI_CREDO) $(LIBSAI_CREDO_OWL) $(LIBSAI_CREDO_BLACKHAWK) $(LIBSAI_CREDO_CRT88322)
+$(DOCKER_GBSYNCD_BASE)_DEPENDS += $(SYNCD) $(LIBSAI_CREDO) $(LIBSAI_CREDO_OWL) $(LIBSAI_CREDO_BLACKHAWK) $(LIBSAI_CREDO_CRT88322)
 endif
