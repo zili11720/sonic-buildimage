739d50b56b9f32df5648f364ff48aa5ca832e85a
Vineet  Mittal <46945843+vmittal-msft@users.noreply.github.com>
2026-01-30 17:32:24 -0800

    Updated DNX SAI version to 14.1.0.1.0.0.5.0 (#25234)
    * Updated DNX SAI version to 14.1.0.1.0.0.5.0

--- platform/broadcom/sai-dnx.mk
+++ platform/broadcom/sai-dnx.mk
@@ -1,5 +1,5 @@
 # Broadcom DNX SAI definitions
-LIBSAIBCM_DNX_VERSION = 14.1.0.1.0.0.0.1
+LIBSAIBCM_DNX_VERSION = 14.1.0.1.0.0.5.0
 LIBSAIBCM_DNX_BRANCH_NAME = SAI_14.1.0_GA
 
 LIBSAIBCM_DNX_URL_PREFIX = "https://packages.trafficmanager.net/public/sai/sai-broadcom/$(LIBSAIBCM_DNX_BRANCH_NAME)/$(LIBSAIBCM_DNX_VERSION)/dnx"
